e08e3878f6bfe58d10cc79f9adf16208db4a2881
SinkingCape <dvdguilfoyle@gmail.com>
2024-07-15 19:51:25 +0000

    recursive loop

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include "mbed.h"
-#include <cstdio>
 
 /*
 next term is the sum of the previous two terms. 
@@ -14,19 +13,33 @@ int next_term = 0;
 int max_terms = 10;
 
 
+int fib(int n){
+    // inp: position of the fib sequence
+    // out: the number in that sequence
+    if(n == 0)
+        return 0;
+    if(n == 1)
+        return 1;
+    else
+        return(fib(n-1) + fib(n-2));
+}
+
 int main()
 {
-    printf("Finbonachi series loop: %d, %d,", t_1, t_2);
-    //for loop
-    for(int i=0; i<max_terms; i++){
+     printf("Finbonachi series loop: %d, %d,", t_1, t_2);
+    for(int i=2; i<max_terms; i++){
          next_term = t_1 + t_2;
          printf("%d, ", next_term);
          t_1 = t_2;
          t_2 = next_term;
      }
      printf("\r\n");
-     printf("Simple Loop");
 
+    printf("Finbonachi series recursive:");
+    for(int i=0; i<max_terms; i++){
+        printf("%d, ", fib(i));
+    }
+    printf("\r\n");
     
     while (true) {
 
